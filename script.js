d6eb8e4eacbf4ef1f3db638c4dcce77752b728b9
cesar pablo gs <ces.studio.arp@gmail.com>
2020-01-27 14:31:22 -0600

    eliminacion de codigo

--- script.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-	navigator.serviceWorker.register('sw.js')
-	.then(reg=>console.log('Registro de sw existoso',reg))
-	.catch(err=>console.warn('Error al tratar de registrar el sw',err))
-}
