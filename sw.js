37f6eb8d07b2d2eee4d26b2d8c33d4a8f08909ba
cesar pablo gs <ces.studio.arp@gmail.com>
2020-04-17 18:00:14 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
-const precacheFiles = [
+const offlineFallbackPage = [
   /* Add an array of files to precache for your app */
 	
   './',
@@ -14,13 +14,14 @@ const precacheFiles = [
   './fonts/icomoon.ttf',
   './fonts/icomoon.woff',
   './css/bootstrap.min.css',
-  './js/app.js'
+  './js/app.js',
+  './offline.html'
 	
 	
 ];
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+// const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
