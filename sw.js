f508d287f46f1aaa5f5026bae5ec848638521a26
cesar pablo gs <ces.studio.arp@gmail.com>
2020-04-17 17:38:33 -0500

    page offline y service worker
    se realizo la pagina offline y se modifico el service worker para cachear los estilos css

--- sw.js
+++ sw.js
@@ -3,6 +3,21 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+	
+  './',
+  'https://fonts.googleapis.com/css?family=Space+Mono:400,700&display=swap',
+  './css/styles.css',
+  './css/style-fonts.css',
+  './fonts/icomoon.eot',
+  './fonts/icomoon.ttf',
+  './fonts/icomoon.woff',
+  './css/bootstrap.min.css',
+  './js/app.js'
+	
+	
+];
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline.html";
